fd4e2a29cdfbbb6e5767c208e33775d15bc6baff
arpit-agr <arpit.agrawal2502@gmail.com>
2025-08-04 10:36:22 +0530

    Disable CSS minification by Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	site: "https://arpit.codes",
 	trailingSlash: "always",
+	vite: {
+		build: {
+			cssMinify: false,
+		},
+	},
 });
