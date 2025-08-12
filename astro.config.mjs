ceb7b5b3374630b79fbeff0c74fa543cdc308f4b
arpit-agr <arpit.agrawal2502@gmail.com>
2025-08-12 13:36:57 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://arpit.codes",
+	site: "https://arpit-codes-portfolio.netlify.app",
 	trailingSlash: "always",
 	vite: {
 		build: {
